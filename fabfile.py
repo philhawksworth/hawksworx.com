0facde3fcda2b2d897c58078d9b6acdfcbc5bd89
Phil Hawksworth <phil.hawksworth@rga.com>
2012-07-23 20:58:11 +0100

    new deploy in fab

--- fabfile.py
+++ fabfile.py
@@ -11,12 +11,11 @@ env.time = tmp_time.strftime("%Y%m%d_%H%M%S")
 
 def deploy():
     """Deploy to github pages"""
-    local('jekyll --no-auto --no-server')
+    local('git checkout gh-pages')
+    local('cp -r _site/* . && rm -rf _site/ && touch .nojekyll')
+
     local('git add .')
     local('git commit -m "deploying latest build as at %(time)s"' %env)
-    local('git branch -D gh-pages')
-    local('git checkout -b gh-pages')
-    local('git filter-branch -f --subdirectory-filter -f site')
-    local('git push origin ghp-ages')
-    local('git checkout master')
+    local('git push origin gh-pages')
+    # local('git checkout master')
     print yellow("Done?")
