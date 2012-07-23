b093ad04c9aae2dc9c6ee314f2daf462e0b6cc01
Phil Hawksworth <phil.hawksworth@rga.com>
2012-07-23 17:45:30 +0100

    Adding CNAME for custom domain

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,9 @@
-# Fabric for deploying the spritecow app
+# Fabric for deploying hawksworx.com on Github Pages
 # import os
 import datetime
 
 from fabric.colors import yellow
 from fabric.api import local, env
-
-# PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-# here = lambda *x: os.path.join(PROJECT_ROOT, *x)
-
-# config = ConfigParser.ConfigParser()
-# config.readfp(open('fabric.cfg'))
-
 
 tmp_time = datetime.datetime.now()
 env.time = tmp_time.strftime("%Y%m%d_%H%M%S")
@@ -23,7 +16,7 @@ def deploy():
     local('git commit -m "deploying latest build as at %(time)s"' %env)
     local('git branch -D gh-pages')
     local('git checkout -b gh-pages')
-    local('git filter-branch --subdirectory-filter -f site')
+    local('git filter-branch -f --subdirectory-filter -f site')
     local('git push origin ghp-ages')
-
+    local('git checkout master')
     print yellow("Done?")
