c5b9eebf89d49b639eb12d02f7279543fd90bf36
Phil Hawksworth <phawksworth@gmail.com>
2017-03-05 14:00:55 +0000

    exclude the test comment

--- comment-exclusions.js
+++ comment-exclusions.js
@@ -2,5 +2,6 @@
 // List IDs of form submissions that should be excluded from the comments data. As a hash, for speedy lookup.
 module.exports = {
     "58bbf9af668674165d061163": true,
-    "58bc14bac4d9cc08f2ffa5aa": true
+    "58bc14bac4d9cc08f2ffa5aa": true,
+    "58bc18fe6686747c59d25f51": true
 };
