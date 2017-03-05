7304cfb408c34e47439b86978a1ef604c643f50b
Phil Hawksworth <phawksworth@gmail.com>
2017-03-05 13:35:36 +0000

    comments logic and netlify env variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp        = require('gulp');
 var gutil       = require('gulp-util');
-var jshint      = require('gulp-jshint'); 
+var jshint      = require('gulp-jshint');
 var uglify      = require('gulp-uglify');
 var cssmin      = require('gulp-cssmin');
 var shell       = require('gulp-shell');
@@ -11,8 +11,11 @@ var fs          = require('fs');
 var http        = require('http');
 var gravatar    = require('gravatar');
 var runSequence = require('run-sequence');
+
 // var htmlreplace = require('gulp-html-replace');
 
+// load environment variables
+require('dotenv').config()
 
 var paths = {
   source: '_site',
@@ -92,39 +95,67 @@ gulp.task("tasks", function() {
 
 // Get comments form Poole
 gulp.task("comments", function() {
-  
+
   console.log("Getting comments data");
 
+  var token = process.env.NETLIFY_TOKEN;
+  var formID = process.env.FORM_ID;
+
   var options = {
-    hostname: 'pooleapp.com',
+    hostname: 'api.netlify.com',
     port: 80,
-    path: '/data/d90ecd8b-f781-4fa6-806f-6a4fbc84a2cc.json',
+    path: '/api/v1/forms/'+ formID +'/submissions?access_token=' + token,
     method: 'GET'
   };
 
   http.get(options, function(res) {
     var body = '';
     res.on('data', function(chunk) {
-        body += chunk;
+      body += chunk;
     });
     res.on('end', function() {
       var comments = JSON.parse(body);
+      var formatted = [];
+      var excluded = {
+        "58bbfc6671e20a278d458bf5": true
+      };
 
-      // add gravatar image links if available
-      for (var i = 0; i < comments.sessions.length; i++) {
-        if(comments.sessions[i].email) {
-          comments.sessions[i].avatar = gravatar.url(comments.sessions[i].email, {s: '50', r: 'pg', d: '404'});
+      // format the comments object into something friendly for saving and serving.
+      for (var i = 0; i < comments.length; i++) {
 
-          if(comments.sessions[i].email.substring(0, 4) == "http") {
-            console.log(comments.sessions[i].email);
-            delete comments.sessions[i];
+        var thisComment = comments[i];
+
+        // exclude any comments flagged for deletion (while Netlify don't support deleting submissions)
+        if(!excluded[thisComment.id]) {
+
+          var formattedComment = {};
+          formattedComment._id = thisComment.id;
+          formattedComment.created = thisComment.created_at;
+          for(var field in thisComment.human_fields) {
+            formattedComment[field.toLowerCase()] = thisComment.human_fields[field];
           }
-        }
+          // add gravatar image links if available
+          if(formattedComment.email) {
+            formattedComment.avatar = gravatar.url(formattedComment.email, {s: '50', r: 'pg'});
+            // delete this regular spam pattern
+            // if(thisComment.human_fields.Email.substring(0, 4) == "http") {
+            //   console.log(thisComment.human_fields.Email);
+            //   delete thisComment.human_fields;
+            // }
+          }
 
+          console.log(formattedComment);
+          formatted.push(formattedComment);
+        }
       }
 
+
+      // include legacy comments for Poole
+      var oldComments = fs.readFileSync('./src/_data/comments-poole.yml', {'encoding': 'utf8'} );
+      console.log(oldComments);
+
       // convert the json to yaml and save it for jekyll to use.
-      var ymlText = yaml.stringify(comments);
+      var ymlText = yaml.stringify(formatted) + oldComments;
       fs.writeFile('./src/_data/comments.yml', ymlText, function(err) {
         if(err) {
           console.log(err);
@@ -142,7 +173,6 @@ gulp.task("comments", function() {
 
 
 
-
 // Build and optimise the site and serve it locally.
 // gulp.task('build', ['jekyll', 'scripts', 'styles', 'configs']);
 
@@ -153,8 +183,6 @@ gulp.task('build', function(callback) {
     callback
   );
 });
-
-
 
 
 
