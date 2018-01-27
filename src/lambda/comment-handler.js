56164e7c8161e77cc065daf97e2789074f6f8cb4
Phil Hawksworth <phil@netify.com>
2018-01-27 17:02:04 +0000

    stub commend function

---
+++ src/lambda/comment-handler.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var request = require("request");
+var oauth_token = process.env.NETLIFY_TOKEN;
+
+export function handler(event, context, callback) {
+
+  console.log(event);
+
+  // get the id of the submission in question from the request
+  // var id = event.queryStringParameters['id'];
+  // var url = "https://api.netlify.com/api/v1/submissions/" +id + "?access_token=" + oauth_token;
+  // console.log("Requesting ", url);
+
+
+  // // get the action we'll perform" [ delete | approve ]
+  // var action = event.queryStringParameters['action'];
+
+  // delete: delete this submission via the api
+
+  // approve: post this comment to the approved comments form and let Netlify trigger a build to include it.
+
+
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  })
+
+
+  // go get it
+  // request(url, function(err, response, body){
+  //   if(!err && response.statusCode === 200){
+  //     console.log("...we got a result");
+  //     callback(null, {
+  //       statusCode: 200,
+  //       body: body
+  //     })
+  //   }
+  // });
+}
+
---
+++ themes/simple-starter/layouts/partials/comment-form.html
@@ -0,0 +1,2 @@
+<form action="">
+</form>
