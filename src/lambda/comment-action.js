fe488d7f4bee8d473806888b689369dd04b68c07
Phil Hawksworth <phil@netify.com>
2018-01-28 19:08:59 +0000

    dummy html cleanup

--- src/lambda/comment-action.js
+++ src/lambda/comment-action.js
@@ -64,24 +64,3 @@ export function handler(event, context, callback) {
   // get the action we'll perform" [ delete | approve ]
   // var action = event.queryStringParameters['action'];
 
-
-  <form name="blog-comment-queue" netlify-honeypot="full-name" action="thanks" netlify>
-  <p class="honey">
-    <label>Your full name: <input name="full-name"></label>
-  </p>
-  <p>
-    <label for="name">Your name<small>What should I call you?.</small></label>
-    <input type="text" name="name" id="email">
-  </p>
-  <p>
-    <label for="email">Your email<small>I'll never spam you. I promise.</small></label>
-    <input type="text" name="email" id="email">
-  </p>
-  <p>
-    <label for="comment">Your comment<small>Markdown is fine.</small></label>
-    <textarea name="comment" id="comment"></textarea>
-  </p>
-  <p>
-    <button type="submit" class="btn">Post your comment</button>
-  </p>
-</form>
