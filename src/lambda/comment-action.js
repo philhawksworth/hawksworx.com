e8aabcb19671e559fbf1c76f5ee321eac9e33aa9
Phil Hawksworth <phil@netify.com>
2018-01-28 14:18:05 +0000

    deletion request

--- src/lambda/comment-action.js
+++ src/lambda/comment-action.js
@@ -1,19 +1,12 @@
 'use strict';
 
 var request = require("request");
-// var oauth_token = process.env.NETLIFY_TOKEN;
-
+var oauth_token = process.env.NETLIFY_TOKEN;
 
 export function handler(event, context, callback) {
 
   // get the action we'll perform" [ delete | approve ]
   // var action = event.queryStringParameters['action'];
-
-  // get the id of the submission in question from the request
-  // var id = event.queryStringParameters['id'];
-
-
-  // var url = "https://api.netlify.com/api/v1/submissions/" +id + "?access_token=" + oauth_token;
 
   // parse the payload
   var body = event.body.split("payload=")[1];
@@ -23,26 +16,28 @@ export function handler(event, context, callback) {
 
   console.log(method, id);
 
-  // delete: delete this submission via the api
+  if(method == "delete") {
+    // delete: delete this submission via the api
+    var url = "https://api.netlify.com/api/v1/submissions/" +id + "?access_token=" + oauth_token;
 
-  // approve: post this comment to the approved comments form and let Netlify trigger a build to include it.
+    request.delete(url, function(err, response, body){
+      if(!err && response.statusCode === 200){
+        callback(null, {
+          statusCode: 200,
+          body: "Comment deleted"
+        })
+      }
+    });
 
+  } else if (method == "approve"){
 
-  callback(null, {
-    statusCode: 200,
-    body: method + ", " + id
-  })
+    // approve: post this comment to the approved comments form and let Netlify trigger a build to include it.
+    callback(null, {
+      statusCode: 200,
+      body: "Comment approved. Site deploying to include it."
+    })
 
+  }
 
-  // go get it
-  // request(url, function(err, response, body){
-  //   if(!err && response.statusCode === 200){
-  //     console.log("...we got a result");
-  //     callback(null, {
-  //       statusCode: 200,
-  //       body: body
-  //     })
-  //   }
-  // });
 }
 
