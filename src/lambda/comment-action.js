d348d0e12b69677261651a4e1627e072c03243a3
Phil Hawksworth <phil@netify.com>
2018-01-28 15:59:33 +0000

    form stubs

--- src/lambda/comment-action.js
+++ src/lambda/comment-action.js
@@ -27,6 +27,31 @@ export function handler(event, context, callback) {
     });
   } else if (method == "approve"){
     // approve: post this comment to the approved comments form and let Netlify trigger a build to include it.
+
+    var commentFormURL = "https://www.hawksworx.com/stubs/comments/thank-you";
+    var messagePayload = {
+      path: "test-path",
+      email: "test-email",
+      name: "test-name",
+      comment: "test-comment"
+    };
+    request.post({url:slackURL, json: messagePayload}, function(err, httpResponse, body) {
+      var msg;
+      if (err) {
+        msg = 'Post to comment stash failed:' + err;
+      } else {
+        msg = 'Post to comment stash successful!  Server responded with:' + body;
+      }
+      callback(null, {
+        statusCode: 200,
+        body: msg
+      })
+      return console.log(msg);
+    });
+
+
+
+
     callback(null, {
       statusCode: 200,
       body: "Comment approved. Site deploying to include it."
@@ -38,3 +63,25 @@ export function handler(event, context, callback) {
 
   // get the action we'll perform" [ delete | approve ]
   // var action = event.queryStringParameters['action'];
+
+
+  <form name="blog-comment-queue" netlify-honeypot="full-name" action="thanks" netlify>
+  <p class="honey">
+    <label>Your full name: <input name="full-name"></label>
+  </p>
+  <p>
+    <label for="name">Your name<small>What should I call you?.</small></label>
+    <input type="text" name="name" id="email">
+  </p>
+  <p>
+    <label for="email">Your email<small>I'll never spam you. I promise.</small></label>
+    <input type="text" name="email" id="email">
+  </p>
+  <p>
+    <label for="comment">Your comment<small>Markdown is fine.</small></label>
+    <textarea name="comment" id="comment"></textarea>
+  </p>
+  <p>
+    <button type="submit" class="btn">Post your comment</button>
+  </p>
+</form>
