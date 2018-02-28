48fc5b89be906a45311a893bde83a93488521ffd
Phil Hawksworth <phil@netify.com>
2018-02-28 20:21:36 +0000

    a more reasonable ab expire date

--- src/js/beta.js
+++ src/js/beta.js
@@ -1,17 +1,17 @@
 
 var btnIN = document.querySelector('#btn-opt-in');
-btnIN.addEventListener('click', function (event) {
+btnIN.addEventListener('click', function(event) {
   event.preventDefault();
   console.log("cookie time");
-  document.cookie = "nf_ab=0.9; expires=Thu, 01 Jan 2070 00:00:00 GMT";
+  document.cookie = "nf_ab=0.9; expires=Thu, 01 Jan 2019 00:00:00 GMT";
   window.location.reload(true);
 }, false);
 
 
 var btnOUT = document.querySelector('#btn-opt-out');
-btnOUT.addEventListener('click', function (event) {
+btnOUT.addEventListener('click', function(event) {
   event.preventDefault();
   console.log("cookie time");
-  document.cookie = "nf_ab=0.1; expires=Thu, 01 Jan 2070 00:00:00 GMT";
+  document.cookie = "nf_ab=0.1; expires=Thu, 01 Jan 2019 00:00:00 GMT";
   window.location.reload(true);
 }, false);
