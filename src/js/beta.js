dacf896ccedf9cead2810b0cac925451eec0ac5c
Phil Hawksworth <phil@netify.com>
2018-02-28 16:29:33 +0000

    fix var clash

--- src/js/beta.js
+++ src/js/beta.js
@@ -1,6 +1,6 @@
 
-var btn = document.querySelector('#btn-opt-in');
-btn.addEventListener('click', function (event) {
+var btnIN = document.querySelector('#btn-opt-in');
+btnIN.addEventListener('click', function (event) {
   event.preventDefault();
   console.log("cookie time");
   document.cookie = "nf_ab=0.9; expires=Thu, 01 Jan 2070 00:00:00 GMT";
@@ -8,8 +8,8 @@ btn.addEventListener('click', function (event) {
 }, false);
 
 
-var btn = document.querySelector('#btn-opt-out');
-btn.addEventListener('click', function (event) {
+var btnOUT = document.querySelector('#btn-opt-out');
+btnOUT.addEventListener('click', function (event) {
   event.preventDefault();
   console.log("cookie time");
   document.cookie = "nf_ab=0.1; expires=Thu, 01 Jan 2070 00:00:00 GMT";
