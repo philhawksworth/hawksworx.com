b1d8664cb92b8074d404ba8f5049b0e13eaf3f46
Phil Hawksworth <phil@netify.com>
2018-02-28 22:30:55 +0000

    reload and no async

--- static/js/hawksworx.js
+++ static/js/hawksworx.js
@@ -1,17 +1,16 @@
-
-var btn = document.querySelector('#btn-opt-in');
-btn.addEventListener('click', function (event) {
+var btnIN = document.querySelector('#btn-opt-in');
+btnIN.addEventListener('click', function(event) {
   event.preventDefault();
-  console.log("cookie time");
-  document.cookie = "nf_ab=0.9; expires=Thu, 01 Jan 2070 00:00:00 GMT";
+  document.cookie = "nf_ab=oh-so-orange; expires=Thu, 01 Jan 2019 00:00:00 GMT";
+  console.log(document.cookie);
   window.location.reload(true);
 }, false);
 
 
-var btn = document.querySelector('#btn-opt-out');
-btn.addEventListener('click', function (event) {
+var btnOUT = document.querySelector('#btn-opt-out');
+btnOUT.addEventListener('click', function(event) {
   event.preventDefault();
-  console.log("cookie time");
-  document.cookie = "nf_ab=0.1; expires=Thu, 01 Jan 2070 00:00:00 GMT";
+  document.cookie = "nf_ab=master; expires=Thu, 01 Jan 2019 00:00:00 GMT";
+  console.log(document.cookie);
   window.location.reload(true);
 }, false);
