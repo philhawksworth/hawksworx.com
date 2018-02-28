1429cc3720b7dd6a017221908c3d4c132359ba72
Phil Hawksworth <phil@netify.com>
2018-02-28 22:17:39 +0000

    explicit branch options

--- src/js/beta.js
+++ src/js/beta.js
@@ -1,7 +1,7 @@
 var btnIN = document.querySelector('#btn-opt-in');
 btnIN.addEventListener('click', function(event) {
   event.preventDefault();
-  document.cookie = "nf_ab=0.9; expires=Thu, 01 Jan 2019 00:00:00 GMT";
+  document.cookie = "nf_ab=oh-so-orange; expires=Thu, 01 Jan 2019 00:00:00 GMT";
   console.log(document.cookie);
   // window.location.reload(true);
 }, false);
@@ -10,7 +10,7 @@ btnIN.addEventListener('click', function(event) {
 var btnOUT = document.querySelector('#btn-opt-out');
 btnOUT.addEventListener('click', function(event) {
   event.preventDefault();
-  document.cookie = "nf_ab=0.1; expires=Thu, 01 Jan 2019 00:00:00 GMT";
+  document.cookie = "nf_ab=master; expires=Thu, 01 Jan 2019 00:00:00 GMT";
   console.log(document.cookie);
   // window.location.reload(true);
 }, false);
