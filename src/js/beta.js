88d0a222b33349137071ca2f3ac0ddb0deb7ee8b
Phil Hawksworth <phil@netify.com>
2018-03-01 10:03:02 +0000

    better click hander and sompler option page

--- src/js/beta.js
+++ src/js/beta.js
@@ -1,16 +1,21 @@
-var btnIN = document.querySelector('#btn-opt-in');
-btnIN.addEventListener('click', function(event) {
-  event.preventDefault();
+// simple button click event handler
+function btnHandler(selector, callback) {
+  var btn = document.querySelector(selector);
+  if(!btn) { return; }
+  btn.addEventListener('click', function(event) {
+    event.preventDefault();
+    callback();
+  }, false);
+}
+
+// Set a cookie to opt in to the beta
+btnHandler('#btn-opt-in', function(){
   document.cookie = "nf_ab=oh-so-orange; expires=Thu, 01 Jan 2019 00:00:00 GMT";
-  console.log(document.cookie);
   window.location.reload(true);
-}, false);
+})
 
-
-var btnOUT = document.querySelector('#btn-opt-out');
-btnOUT.addEventListener('click', function(event) {
-  event.preventDefault();
+// Set a cookie to opt out of the beta
+btnHandler('#btn-opt-out', function(){
   document.cookie = "nf_ab=master; expires=Thu, 01 Jan 2019 00:00:00 GMT";
-  console.log(document.cookie);
   window.location.reload(true);
-}, false);
+})
