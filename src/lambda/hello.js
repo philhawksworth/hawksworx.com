c2440dc3bdf8fdc7da58b9f929ee0073bcdf5b22
Phil Hawksworth <phil@netify.com>
2018-05-13 13:27:28 +0100

    defaults and comments

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,18 +1,30 @@
+/**
+ *  This is a serverless function  which runs in AWS Lambda.
+ *  It is built for use on Netlify so that building, versioning
+ *  and deploying is easier than it might be otherwise.
+ *
+ *  No need to configure AWS, just include a file like this in
+ *  your site, and deploy as you would any other site on Netlify.
+ *
+ *  https://www.netlify.com/docs/functions/
+ */
+
 'use strict';
+
 
 // A standard Lambda function handler
 export function handler(event, context, callback) {
 
-  console.log('queryStringParameters :', JSON.stringify(event.queryStringParameters));
-
   // who's there?
-  const  { name }  =  event.queryStringParameters ;
+  const  { name: name = "World" } = event.queryStringParameters ;
 
   // Say hello
   callback(null, {
     statusCode: 200,
-    body: `Hello ${name}!`
+    headers: { "Content-Type": "text/html" },
+    body: `<html>Hello from a serverless
+      <a href="https://github.com/philhawksworth/hawksworx.com/blob/master/src/lambda/hello.js">function</a>
+      , ${name}!</hml>`
   });
 
 }
-
