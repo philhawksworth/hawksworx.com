a8c0ac14790c6ee65723eb1d57ea7fd820512b5d
Phil Hawksworth <phil@netify.com>
2018-05-20 18:53:24 +0100

    except filter fix

--- filters/excerpt.js
+++ filters/excerpt.js
@@ -4,7 +4,7 @@
 module.exports = function(str) {
   var content = new String(str);
   var delimit = "\n<!--more-->\n";
-  var parts = content.split(delimit)[0];
+  var parts = content.split(delimit);
   if(parts.length) {
     return parts[0];
   } else {
