196f2f2a5e27a2f9521de0e159de23f153c6bac8
Phil Hawksworth <phil@netify.com>
2018-08-07 14:21:45 +0100

    trigger commit

--- gulp-tasks/debug.js
+++ gulp-tasks/debug.js
@@ -3,6 +3,6 @@ var gulp    = require('gulp');
 
 // Get the latest few tweets to include in some pages
 gulp.task('debug', function() {
-    console.log("commit");
+    console.log("process.env.COMMIT_REF");
     console.log(process.env.COMMIT_REF);
 });
