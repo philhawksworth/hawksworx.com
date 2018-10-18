ca924a39f0d5041966e2976641523383ee394aae
Phil Hawksworth <phil@netify.com>
2018-10-18 13:13:47 +0100

    update readme and tudy eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,22 @@
-const htmlmin = require("html-minifier");
-
 module.exports = function(eleventyConfig) {
-
-  // Components
-  // const PostTitle = require('./src/site/_includes/components/PostTitle.js');
-  // const PostTitle = require('./src/site/_includes/components/PostTeaser.js');
-
-  // Shortcodes
-  // eleventyConfig.addShortcode('PostTitle', PostTitle);
-  // eleventyConfig.addShortcode('PostTeaser', PostTeaser);
 
   // Add filters to Nunjucks
   eleventyConfig.addFilter("dateDisplay", require("./src/site/_filters/dates.js") );
   eleventyConfig.addFilter("section", require("./src/site/_filters/section.js") );
   eleventyConfig.addFilter("squash", require("./src/site/_filters/squash.js") );
 
-  // Group posts into a collection without leaning on tags
+  // Assemble some collections
+  eleventyConfig.addCollection("tagList", require("./src/site/_filters/getTagList.js"));
   eleventyConfig.addCollection("posts", function(collection) {
     return collection.getFilteredByGlob("src/site/blog/*.md").reverse();
   });
-
-  eleventyConfig.addCollection("tagList", require("./src/site/_filters/getTagList.js"));
 
   // static passthroughs
   eleventyConfig.addPassthroughCopy("src/site/fonts");
   eleventyConfig.addPassthroughCopy("src/site/images");
 
   // minify the html ouptput
+  const htmlmin = require("html-minifier");
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
     if( outputPath.endsWith(".html") ) {
       let minified = htmlmin.minify(content, {
@@ -39,6 +29,7 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  // otehr config settings
   return {
     dir: {
       input: "src/site",
