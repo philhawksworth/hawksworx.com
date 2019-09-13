6978a80c96c52728b6c74b1555b4f15b791e8026
Phil Hawksworth <phil@netlify.com>
2019-09-13 11:19:58 +0100

    Add utility for new link entries

---
+++ utils/new.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth() + 1;
+const yyyy = today.getFullYear();
+if (dd < 10) {
+  dd = '0' + dd;
+}
+if (mm < 10) {
+  mm = '0' + mm;
+}
+const datestamp = dd + '-' + mm + '-' + yyyy;
+
+// get the meaningful arguments
+process.argv.splice(0,2);
+
+if(process.argv[0] == 'link') {
+  const template = require('./templates/link.js');
+  process.argv.splice(0,1);
+
+  let props = {
+    title: process.argv.join("-").toLowerCase(),
+    date: datestamp
+  }
+
+  let path = `src/site/blog/${props.title}.md`;
+  let fileContent = template(props);
+
+  fs.writeFile(path, fileContent, err => {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(`New file created: ${path}`);
+    }
+  });
+
+} else {
+  console.log('Unknown file template');
+};
+
+
---
+++ utils/templates/link.js
@@ -0,0 +1,13 @@
+
+module.exports = (data) => `---
+permalink: null
+title: ${data.title}
+date: ${data.date}
+tags:
+  - some
+  - tags
+externalurl: ADD URL
+---
+
+SHORT DESCRIPTION
+`;
