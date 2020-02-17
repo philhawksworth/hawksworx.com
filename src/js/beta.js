3d14319244d3fe62ba9f06113d5e4efcff5fb2cf
Phil Hawksworth <phil@netlify.com>
2020-02-17 11:30:45 +0000

    remove test

--- src/js/beta.js
+++ src/js/beta.js
@@ -7,17 +7,6 @@ btnHandler('#btn-opt-in', function(){
   window.location.reload(true);
 })
 
-
-// Set a cookie to opt in to the beta
-btnHandler('#btn-opt-in-slash', function(){
-  var now = new Date();
-  var expires = now.getTime() + 1000 * 3600 * 24 * 365;
-  now.setTime(expires);
-  document.cookie = 'nf_ab=test/slashes-in-branches; expires=' + now.toUTCString();
-  window.location.reload(true);
-})
-
-
 // Set a cookie to opt out of the beta
 btnHandler('#btn-opt-out', function(){
   document.cookie = "nf_ab=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
