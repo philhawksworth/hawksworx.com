3b2975a1108e32a9fce6f8cdd1f1c8b8955166dc
Phil Hawksworth <phil@netlify.com>
2022-12-05 12:54:51 +0000

    logging in hello EF

--- netlify/edge-functions/eleventy-edge.js
+++ netlify/edge-functions/eleventy-edge.js
@@ -3,6 +3,8 @@ import {
   precompiledAppData,
 } from "./_generated/eleventy-edge-app.js";
 
+import notes from "../../src/site/_data/notes.json" assert {type: 'json'}; 
+
 export default async (request, context) => {
   try {
     let edge = new EleventyEdge("edge", {
@@ -13,11 +15,25 @@ export default async (request, context) => {
       // default is [], add more keys to opt-in e.g. ["appearance", "username"]
       cookies: [],
     });
+    
+    const url = new URL(request.url);
+    const searchStr = url.searchParams.get("str");
+    let filteredNotes = {
+      filter: searchStr,
+      results: []
+    };
+    if(searchStr) {
+      console.log("search for ", searchStr );
+      filteredNotes.results = notes.filter(e => e.full_text.indexOf(searchStr) !== -1);
+    }
 
+    console.log(`results: ${filteredNotes.results.length}`);
+    
+    
     edge.config((eleventyConfig) => {
       // Add some custom Edge-specific configuration
-      // e.g. Fancier json output
-      // eleventyConfig.addFilter("json", obj => JSON.stringify(obj, null, 2));
+      eleventyConfig.addGlobalData("filteredNotes", filteredNotes);
+      eleventyConfig.addGlobalData("notes", notes);
     });
 
     return await edge.handleResponse();
--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,4 +1,7 @@
-export default async (request) => {
+export default async (request, context) => {
+
+  context.log("Hello from an edge function");
+
   return new Response("Hello, World!", {
     headers: { "content-type": "text/html" },
   });
