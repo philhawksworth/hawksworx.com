5679e20aaa8cc23bfd8cc3ca8ef9a449f18d3de3
Phil Hawksworth <phil@netlify.com>
2024-03-30 10:24:34 +0000

    Remove auto p tag around md images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import remarkUnwrapImages from 'remark-unwrap-images'
 
 export default defineConfig({
   output: 'static',
   integrations: [mdx()],
-  // markdown: {
-  //   // Applied to .md and .mdx files
-  //   remarkPlugins: [markdownItAttrs],
-  // },
+  markdown: {
+    remarkPlugins: [remarkUnwrapImages],
+  },
 });
 
 
