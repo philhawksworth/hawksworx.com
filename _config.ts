a2f12d7f4921dbeb735606f3b7d5fdec518ac395
Phil Hawksworth <phil@netlify.com>
2024-07-11 08:49:53 +0100

    Ad date formatting

--- _config.ts
+++ _config.ts
@@ -1,13 +1,19 @@
 import lume from "lume/mod.ts";
 import sass from "lume/plugins/sass.ts";
+import date from "lume/plugins/date.ts";
 
+// Config
 const site = lume({
   src: "./src",
   dest: "./_site",
 });
 
+// Plugins
+site.use(date());
 site.use(sass({"format": "compressed"}));
 
+
+// Filters and helpers
 site.filter("contentExcerpt", (value) => value.split("<!--more-->")[0]);
 site.filter("contentAfterExcerpt", (value) => value.split("<!--more-->")[1]);
 
