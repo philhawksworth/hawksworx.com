fbe1c5cf63fc6f0be2df69077cc1c3fb9dae3682
Phil Hawksworth <phil@netlify.com>
2024-08-09 10:24:58 +0100

    notes listing pages styles

--- src/notes.page.js
+++ src/notes.page.js
@@ -1,12 +1,10 @@
 export const layout = "layouts/notes-list.vto";
 
 export default function* ({ notes, paginate }) {
-    
   const options = {
     url: (n) => n == 1 ? `/notes/` : `/notes/${n}/`,
     size: 50,
   };
-
   for (const page of paginate(notes, options)) {
     yield page;
   }
